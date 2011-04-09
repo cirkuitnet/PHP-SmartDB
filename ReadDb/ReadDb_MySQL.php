61fa8dfad25b58f044ea6756b2cc85e5981ae30c
Cirkuit Networks <sales@cirkuit.net>
2011-04-09 09:45:13 -0400

    Adding the new ReadDb_MySQL class (got missed with the previous commit)

---
+++ ReadDb/ReadDb_MySQL.php
@@ -0,0 +1,191 @@
+<?php
+/*!
+ * PHP SmartDb
+ * http://www.phpsmartdb.com/
+ *
+ * Copyright 2011, Cirkuit Networks
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://www.phpsmartdb.com/license
+ */
+/**
+ * @package SmartDatabase
+ */
+/**
+ * This class will update a database to match a given db structure schema (described below)
+ * 
+ * -- db structure layout guide --
+ * <code>
+ * array(
+ * 	"<table name>"=>array(
+ * 		"<column name>"=>array(
+ * 			"Field"=>"<column name>",
+ * 			"Type"=>"<column type>",
+ * 			"Null"=>"<YES|NO>",
+ * 			"Key"=>"<PRI|UNI|MUL|empty>", //note: PRI=Primary Key, UNI=Unique index, MUL=multiple... seems to mean the same as UNI though
+ * 			"Default"=>"<default value|empty>",
+ * 			"Extra"=>"<auto_increment|empty>",
+ * 			"Collation"=>"<utf8_general_ci|latin1_swedish_ci|empty>" //other collations can easily be added if needed
+ * 		),
+ * 		...(more columns)...
+ * 	),
+ * 	...(more tables)...
+ * )
+ * </code>
+ * 
+ * --- EXAMPLE ---
+ * <code>
+ * function getDbStructure(){
+ * 	return array(
+ * 		"Template"=>array(
+ * 			"TemplateId"=>array(
+ * 				"Field"=>"TemplateId",
+ * 				"Type"=>"int(1) unsigned",
+ * 				"Null"=>"NO",
+ * 				"Key"=>"PRI",
+ * 				"Default"=>"",
+ * 				"Extra"=>"auto_increment",
+ *				"Collation"=>"",
+ * 			),
+ * 			"Name"=>array(
+ * 				"Field"=>"Name",
+ * 				"Type"=>"varchar(255)",
+ * 				"Null"=>"NO",
+ * 				"Key"=>"UNI",
+ * 				"Default"=>"",
+ * 				"Extra"=>"",
+ *				"Collation"=>"",
+ * 			),
+ * 			"Html"=>array(
+ * 				"Field"=>"Html",
+ * 				"Type"=>"text",
+ * 				"Null"=>"YES",
+ * 				"Key"=>"",
+ * 				"Default"=>"",
+ * 				"Extra"=>"",
+ *				"Collation"=>"utf8_general_ci",
+ * 			)
+ * 		)
+ * );
+ * </code>
+ * Reads a database structure properties that the SmartDatabase can use to load/write XML schemas
+ * @package SmartDatabase
+ */
+class ReadDb_MySQL{
+	
+	// Hold an instance of the class
+    private static $instance;
+
+	//only allow the singleton to access this class
+	private function __construct(){
+    }
+
+    // The singleton method
+    public static function Instance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+	
+	/**
+	 * Returns detailed array with all columns for given table in database, or all tables/databases
+	 * Only works with PMA_MYSQL_INT_VERSION >= 50002!
+	 * ORIGINAL SOURCE - phpMyAdmin-2.11.11.3 - function PMA_DBI_get_columns_full()
+	 * 
+	 * <code>
+	 * //the returned array must contain at least this information
+	 * $return = array(
+	 * 	"TABLE NAME" => array(
+	 * 		"COLUMN NAME 1" => array(
+ 	 * 			"Field"=>"<column name>",
+ 	 * 			"Type"=>"<column type>",
+ 	 * 			"Null"=>"<YES|NO>",
+ 	 * 			"Key"=>"<PRI|UNI|MUL|empty>", //note: PRI=Primary Key, UNI=Unique index, MUL=multiple... seems to mean the same as UNI though
+ 	 * 			"Default"=>"<default value|empty>",
+	 * 			"Extra"=>"<auto_increment|empty>",
+ 	 * 			"Collation"=>"<utf8_general_ci|latin1_swedish_ci|empty>", //other collations can easily be added if needed
+ 	 * 			"IndexType"=>"UNIQUE|FULLTEXT|empty",
+	 *		),
+	 *		"COLUMN NAME 2" => array(
+	 *		...etc...
+	 *	),
+	 * );
+	 * </code>
+	 *
+	 * @param   string  $database   name of database
+	 * @param   string  $table      name of table to retrieve columns from
+	 * @param   string  $column     name of specific column
+	 */
+	function GetArray($dbManager, $database = null, $table = null, $column = null){
+	    $columns = array();
+	
+        $sql_wheres = array();
+        $array_keys = array();
+
+        // get columns information from information_schema
+        if (null !== $database) {
+            $sql_wheres[] = '`TABLE_SCHEMA` = \'' . addslashes($database) . '\' ';
+        } else {
+            $array_keys[] = 'TABLE_SCHEMA';
+        }
+        if (null !== $table) {
+            $sql_wheres[] = '`TABLE_NAME` = \'' . addslashes($table) . '\' ';
+        } else {
+            $array_keys[] = 'TABLE_NAME';
+        }
+        if (null !== $column) {
+            $sql_wheres[] = '`COLUMN_NAME` = \'' . addslashes($column) . '\' ';
+        } else {
+            $array_keys[] = 'COLUMN_NAME';
+        }
+
+        // for PMA bc:
+        // `[SCHEMA_FIELD_NAME]` AS `[SHOW_FULL_COLUMNS_FIELD_NAME]`
+        $sql = 'SELECT `TABLE_SCHEMA`		AS `TableSchema`,
+             		`TABLE_NAME`		AS `TableName`,
+                    `COLUMN_NAME`       AS `Field`,
+                    `COLUMN_TYPE`       AS `Type`,
+                    `COLLATION_NAME`    AS `Collation`,
+                    `IS_NULLABLE`       AS `Null`,
+                    `COLUMN_KEY`        AS `Key`,
+                    `COLUMN_DEFAULT`    AS `Default`,
+                    `EXTRA`             AS `Extra`,
+                    `PRIVILEGES`        AS `Privileges`,
+                    `COLUMN_COMMENT`    AS `Comment`
+               FROM `information_schema`.`COLUMNS`';
+        if (count($sql_wheres)) {
+            $sql .= "\n" . ' WHERE ' . implode(' AND ', $sql_wheres);
+        }
+
+        $dbManager->Query($sql);
+        $rows = $dbManager->FetchAssocList();
+        
+        $results = array();
+        foreach($rows as $row){
+        	$tableName = $row['TableName'];
+        	$columnName = $row['Field'];
+        	$results[$tableName][$columnName] = $row;
+        }
+        
+        //lookup indexes on all tables
+        foreach($results as $tableName => $colArr){
+        	$dbManager->Query('SHOW INDEX FROM `'.$tableName.'`');
+        	$rows = $dbManager->FetchAssocList();
+        	foreach($rows as $row){
+        		$colName = $row['Column_name'];
+        		
+        		if($row['Index_type'] == "FULLTEXT"){
+        			$results[$tableName][$colName]['IndexType'] = "FULLTEXT";
+        		}
+        		else{ //i.e. $row['Index_type']=="BTREE"
+        			if(!$results[$tableName][$colName]['Key']){ //if key is not yet set, we need it
+       					$results[$tableName][$colName]['Key'] = "UNI";
+        			}
+        			$results[$tableName][$colName]['IndexType'] = "UNIQUE";
+        		}
+        	}
+        }
+	
+	    return $results;
+	}
+}
